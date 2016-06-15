31fb87f49220b02406a49f082b6171f6284b98fa
affeder <alison.feder@gmail.com>
2016-06-14 21:35:52 -0700

    Added file to read in the data
    File reads in data and looks at the column names

---
+++ code/tmpdoc.r
@@ -0,0 +1,7 @@
+                                        #tmp document
+
+a <- read.table("../data/data.2015.12.17.addedcols.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+names(a)
+
+
